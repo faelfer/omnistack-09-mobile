5485adeae660fc340f823af0818a99678325c705
NYDino <rfapp00@gmail.com>
2019-10-15 23:12:00 -0300

    :sparkles: List screen finished

--- src/Pages/List.js
+++ src/Pages/List.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Image, StyleSheet, AsyncStorage } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+import SpotList from '../components/SpotLIst';
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    conatainer: {
+        flex: 1
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 10
+    }
+});
